faa5323aea0f9a2c1d870f735e6233db7a8ff8fb
Ubuntu <ubuntu@192.168.88.107>
2018-06-30 16:23:01 +0800

    add comment

--- change.py
+++ change.py
@@ -12,8 +12,10 @@ def cvtBackground(path,color):
     im_hsv=cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
     aim=np.uint8([[im[0,0,:]]])
     hsv_aim=cv2.cvtColor(aim,cv2.COLOR_BGR2HSV)
+    # 找出白色的点
     mask=cv2.inRange(im_hsv,np.array([hsv_aim[0,0,0],0,221]),np.array([hsv_aim[0,0,0]+180,30,255]))
-
+    
+    # 调整前景与背景相似的像素点
     mask[100:, 120:240] = 0
     mask[384:, 270: 347] = 0
     mask_inv=cv2.bitwise_not(mask)
@@ -25,6 +27,7 @@ def cvtBackground(path,color):
     img=cv2.add(img1,img2)
     kernel_size = (3, 3);
     sigma = 1.5;
+    # 高斯模糊是为了去除前景边缘的白边
     img = cv2.GaussianBlur(img, kernel_size, sigma);
     image={'im':im,'im_1':img1, 'im_2':img2, 'im_hsv':im_hsv,'mask':mask,'img':img}
     cv2.imwrite("/home/ubuntu/mt.jpg", img)
@@ -35,4 +38,5 @@ def cvtBackground(path,color):
     return img
 #test
 if __name__=='__main__':
+    # 天蓝色的bgr是[235,206,135]
     img=cvtBackground('/home/ubuntu/ss.jpg',[235,206,135])
